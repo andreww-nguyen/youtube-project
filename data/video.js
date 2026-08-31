2271750dfa0d4b985ecf0f878959e5666b03d162
Andrew Nguyen <andreww.nnguyen@gmail.com>
2026-08-31 14:24:12 -0700

    completed video array

--- data/video.js
+++ data/video.js
@@ -1,7 +1,7 @@
 class Video
 {
   #videoCode;
-  #thumbnailImage;
+  #thumbnail;
   #title;
   #length;
   #creator;
@@ -12,7 +12,7 @@ class Video
   constructor(videoDetails)
   {
     this.#videoCode = videoDetails.code;
-    this.#thumbnailImage = videoDetails.thumbnailImage;
+    this.#thumbnail = videoDetails.thumbnail;
     this.#title = videoDetails.title;
     this.#length = videoDetails.length;
     this.#creator = videoDetails.creator;
@@ -31,7 +31,7 @@ export let videos =
 [
   {
     code: 'RjfQJlMeh1o',
-    thumbnailImage: 'german-reacts-to-car-chase.avif',
+    thumbnail: 'german-reacts-to-car-chase.avif',
     title: 'German Reacts to the Funniest American Police Chase Ever',
     length: '28:09',
     creator: 'ohnepixel raw',
@@ -39,10 +39,21 @@ export let videos =
     views: '119k',
     dateReleased: '2026-06-11'
   },
+  
+  {
+    code: 'lyr5ff5MlQc',
+    thumbnail: 'scariest-co-op-game.avif',
+    title: 'The Scariest Co-op Game I Have Ever Played',
+    length: '2:21:51',
+    creator: 'ohnepixel raw',
+    creatorPfp: 'ohnepixel-raw.jpg',
+    views: '192k',
+    dateReleased: '2026-06-16'
+  },
 
   {
     code: 'k9KUUOjbYt4',
-    thumbnailImage: 'streaming-peak.avif',
+    thumbnail: 'streaming-peak.avif',
     title: 'I streamed PEAK until we beat it... It was a disaster...',
     length: '2:04:27',
     creator: 'ohnepixel raw',
@@ -53,7 +64,7 @@ export let videos =
 
   {
     code: '1Rmnnu_ZDL8',
-    thumbnailImage: 'plays-forza-for-first-time.avif',
+    thumbnail: 'plays-forza-for-first-time.avif',
     title: 'Driving Noob Plays Forza For The First Time Ever...',
     length: '1:11:55',
     creator: 'ohnepixel raw',
@@ -64,14 +75,223 @@ export let videos =
 
   {
     code: '4GqFdf3gFfo',
-    thumbnailImage: 'react-to-skibidi-toilet.avif',
+    thumbnail: 'react-to-skibidi-toilet.avif',
     title: "I watched the entirety of skibidi toilet, it's not what you expect",
-    length: '1:11:55',
+    length: '3:27:08',
+    creator: 'ohnepixel',
+    creatorPfp: 'ohnepixel.jpg',
+    views: '953k',
+    dateReleased: '2025-01-02'
+  },
+
+  {
+    code: 'KLmDAH0ppp8',
+    thumbnail: 'hawaii-vs-japan.avif',
+    title: "I watched the entirety of skibidi toilet, it's not what you expect",
+    length: '23:56',
+    creator: 'vb K Har',
+    creatorPfp: 'vbK.jpg',
+    views: '228k',
+    dateReleased: '2023-11-15'
+  },
+
+  {
+    code: 'WU0GteluwQc&t',
+    thumbnail: 'lbsu-vs-ucla.avif',
+    title: "Long Beach vs UCLA 2025 Men's Volleyball",
+    length: '26:12',
+    creator: 'moomoocowjonny',
+    creatorPfp: 'moomoocowjonny.jpg',
+    views: '738k',
+    dateReleased: '2025-02-08'
+  },
+
+  {
+    code: 'TLw-K5OZOHo&t',
+    thumbnail: 'minecreaft-with-horror-mods.avif',
+    title: "Minecraft, But We Installed Way Too Many Horror Mods",
+    length: '59:48',
     creator: 'ohnepixel raw',
     creatorPfp: 'ohnepixel-raw.jpg',
-    views: '415k',
-    dateReleased: '2025-01-02'
-  }
+    views: '422k',
+    dateReleased: '2026-04-06'
+  },
+
+  {
+    code: 'flXs_joCP9c',
+    thumbnail: 'challenger-coaching-league.avif',
+    title: "Challenger Player Has a Meltdown Trying to Coach Jynxzi & ohnepixel",
+    length: '1:55:30',
+    creator: 'ohnepixel',
+    creatorPfp: 'ohnepixel.jpg',
+    views: '87k',
+    dateReleased: '2026-05-21'
+  },
+
+  {
+    code: 'Zrua1Qvv0Nw',
+    thumbnail: 'opening-tf2-cases.avif',
+    title: "ohnepixel gets insanely lucky opening TF2 cases",
+    length: '21:27',
+    creator: 'ohnepixel raw',
+    creatorPfp: 'ohnepixel-raw.jpg',
+    views: '1.2M',
+    dateReleased: '2023-11-13'
+  },
+
+  {
+    code: 'qjXJsr7QHGZKhj9c',
+    thumbnail: 'valorant-ohnepixel.avif',
+    title: "every time Valorant shocked ohnepixel",
+    length: '35:32',
+    creator: 'ohnepixel',
+    creatorPfp: 'ohnepixel.jpg',
+    views: '1.2M',
+    dateReleased: '2025-10-09'
+  },
+
+  {
+    code: 'zHFsy2tUgRZM_lSw',
+    thumbnail: 'geoguesser-noob-with-ai.avif',
+    title: "can a geoguessr noob beat a geoguessr pro using ai?",
+    length: '46:21',
+    creator: 'ohnepixel raw',
+    creatorPfp: 'ohnepixel-raw.jpg',
+    views: '727k',
+    dateReleased: '2025-10-13'
+  },
+
+  {
+    code: 'C01uehFQoHo',
+    thumbnail: 'grow-a-garden-with-admin.avif',
+    title: "I Play Grow a Garden, but the Admin Trolls Me",
+    length: '1:07:52',
+    creator: 'ohnepixel',
+    creatorPfp: 'ohnepixel.jpg',
+    views: '627k',
+    dateReleased: '2025-07-16'
+  },
+
+  {
+    code: 'sJw8Yt1M35I',
+    thumbnail: 'knife-trade-ups.avif',
+    title: "ohnepixel loses his mind doing knife trade-ups",
+    length: '39:42',
+    creator: 'ohnepixel raw',
+    creatorPfp: 'ohnepixel-raw.jpg',
+    views: '464k',
+    dateReleased: '2025-12-22'
+  },
+
+  {
+    code: 'obCCyKwjMUs',
+    thumbnail: 'uci-vs-lbsu.avif',
+    title: "UCI vs Long Beach 2025 REMATCH",
+    length: '16:29',
+    creator: 'moomoocowjonny',
+    creatorPfp: 'moomoocowjonny.jpg',
+    views: '225k',
+    dateReleased: '2025-04-02'
+  },
+
+  {
+    code: 'LOBDsnLe2wc',
+    thumbnail: 'ohnepixel-rplace.avif',
+    title: "This Unhinged r/place Experiment Has Been Going on for Years...",
+    length: '10:52',
+    creator: 'ohnepixel raw',
+    creatorPfp: 'ohnepixel-raw.jpg',
+    views: '112k',
+    dateReleased: '2026-05-22'
+  },
+
+  {
+    code: 'WbUoomI6i2Q',
+    thumbnail: 'ucla-vs-usc.avif',
+    title: "UCLA at USC 2026",
+    length: '29:38',
+    creator: 'moomoocowjonny',
+    creatorPfp: 'moomoocowjonny.jpg',
+    views: '101k',
+    dateReleased: '2026-03-04'
+  },
+
+  {
+    code: 'uMpo11sFuUs',
+    thumbnail: 'rating-viewer-bases.avif',
+    title: "Rating My Viewers Bases, But I Keep Getting Trolled",
+    length: '1:37:04',
+    creator: 'ohnepixel',
+    creatorPfp: 'ohnepixel.jpg',
+    views: '370k',
+    dateReleased: '2026-04-25'
+  },
+
+  {
+    code: '9636GP0uuxo',
+    thumbnail: 'teresaki-tournament.avif',
+    title: "did we FINALLY do it? (Teresaki Tournament)",
+    length: '33:29',
+    creator: 'Isaiah Espinoza',
+    creatorPfp: 'isaiah-pfp.jpg',
+    views: '86k',
+    dateReleased: '2026-01-05'
+  },
+
+  {
+    code: '1N-TaPybEOQ',
+    thumbnail: 'beating-elden-ring.avif',
+    title: "I tried beating Elden Ring. It was a mistake...",
+    length: '4:46:26',
+    creator: 'ohnepixel raw',
+    creatorPfp: 'ohnepixel-raw.jpg',
+    views: '395k',
+    dateReleased: '2024-07-09'
+  },
+
+  {
+    code: 'MxV5Sjm5xok',
+    thumbnail: 'ohnepixel-famous-clips.avif',
+    title: "even more clips that made ohnepixel famous",
+    length: '18:14',
+    creator: 'ohnepixel raw',
+    creatorPfp: 'ohnepixel-raw.jpg',
+    views: '960k',
+    dateReleased: '2026-02-24'
+  },
+
+  {
+    code: 'GZbsPEaroTw',
+    thumbnail: '2-vs-1-rocket-league.avif',
+    title: "2 Noobs vs 1 Pro, But We Make it Impossible For Him...",
+    length: '1:51:43',
+    creator: 'ohnepixel',
+    creatorPfp: 'ohnepixel.jpg',
+    views: '119k',
+    dateReleased: '2026-05-23'
+  },
+
+  {
+    code: 'LIWZcsyC65k',
+    thumbnail: 'fixing-forza-wheel.avif',
+    title: "ohnepixel Loses It Trying to Fix His NEW Wheel For 3 Hours Again...",
+    length: '46:06',
+    creator: 'ohnepixel raw',
+    creatorPfp: 'ohnepixel-raw.jpg',
+    views: '166k',
+    dateReleased: '2026-05-26'
+  },
+
+  {
+    code: 'fRsUVoJEC2A',
+    thumbnail: 'bounciest-team.avif',
+    title: "The BOUNCIEST Open Gym Volleyball Team that couldn't win?",
+    length: '20:22',
+    creator: 'MK3Volleyball',
+    creatorPfp: 'mk3volleyball.jpg',
+    views: '61k',
+    dateReleased: '2026-05-28'
+  },
 ]
 
 videos = videos.map((videoDetails) =>
--- scripts/youtube.js
+++ scripts/youtube.js
@@ -2,41 +2,41 @@ import { videos } from '../data/video.js'
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 
 
-console.log(dayjs(videos[0].getDateReleased()));
+console.log(videos);
 
 
-const date1 = dayjs('2026-08-16');
-const today = dayjs();
+// const date1 = dayjs('2026-08-16');
+// const today = dayjs();
 
-// Get difference in days
-let timeBetween = today.diff(date1, 'day');
-let timeFormat = 'days'
+// // Get difference in days
+// let timeBetween = today.diff(date1, 'day');
+// let timeFormat = 'days'
 
-console.log(timeBetween);
-if (timeBetween > 365)
-{
-  timeBetween /= 365;
-  timeFormat = 'year';
-}
-else if (timeBetween < 365 && timeBetween > 30)
-{
-  timeBetween /= 30;
-  timeFormat = 'month';
-}
-else if (timeBetween < 30 && timeBetween > 7)
-{
-  timeBetween /= 7;
-  timeFormat = 'week';
-}
-else if (timeBetween < 7)
-{
-  timeFormat = 'day';
-}
+// console.log(timeBetween);
+// if (timeBetween > 365)
+// {
+//   timeBetween /= 365;
+//   timeFormat = 'year';
+// }
+// else if (timeBetween < 365 && timeBetween > 30)
+// {
+//   timeBetween /= 30;
+//   timeFormat = 'month';
+// }
+// else if (timeBetween < 30 && timeBetween > 7)
+// {
+//   timeBetween /= 7;
+//   timeFormat = 'week';
+// }
+// else if (timeBetween < 7)
+// {
+//   timeFormat = 'day';
+// }
 
-// grammar rules
-if (timeBetween !== 1)
-{
-  timeFormat += 's';
-}
+// // grammar rules
+// if (timeBetween !== 1)
+// {
+//   timeFormat += 's';
+// }
 
-console.log(`${Math.floor(timeBetween)} ${timeFormat}`)
+// console.log(`${Math.floor(timeBetween)} ${timeFormat}`)
