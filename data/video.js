1bd760d0cccb45870504209ad983da416db78ca6
Andrew Nguyen <andreww.nnguyen@gmail.com>
2026-08-31 10:55:58 -0700

    added logic for calculating time difference

--- data/video.js
+++ data/video.js
@@ -20,19 +20,57 @@ class Video
     this.#views = videoDetails.views;
     this.#dateReleased = videoDetails.dateReleased;
   }
+
+  getDateReleased()
+  {
+    return this.#dateReleased;
+  }
 }
 
 export let videos =
 [
   {
-    code: 'v=1Rmnnu_ZDL8',
+    code: 'RjfQJlMeh1o',
+    thumbnailImage: 'german-reacts-to-car-chase.avif',
+    title: 'German Reacts to the Funniest American Police Chase Ever',
+    length: '28:09',
+    creator: 'ohnepixel raw',
+    creatorPfp: 'ohnepixel-raw.jpg',
+    views: '119k',
+    dateReleased: '2026-06-11'
+  },
+
+  {
+    code: 'k9KUUOjbYt4',
+    thumbnailImage: 'streaming-peak.avif',
+    title: 'I streamed PEAK until we beat it... It was a disaster...',
+    length: '2:04:27',
+    creator: 'ohnepixel raw',
+    creatorPfp: 'ohnepixel-raw.jpg',
+    views: '205',
+    dateReleased: '2025-07-10'
+  },
+
+  {
+    code: '1Rmnnu_ZDL8',
     thumbnailImage: 'plays-forza-for-first-time.avif',
     title: 'Driving Noob Plays Forza For The First Time Ever...',
     length: '1:11:55',
     creator: 'ohnepixel raw',
     creatorPfp: 'ohnepixel-raw.jpg',
     views: '415k',
-    dateReleased: '4 weeks ago'
+    dateReleased: '2026-05-27'
+  },
+
+  {
+    code: '4GqFdf3gFfo',
+    thumbnailImage: 'react-to-skibidi-toilet.avif',
+    title: "I watched the entirety of skibidi toilet, it's not what you expect",
+    length: '1:11:55',
+    creator: 'ohnepixel raw',
+    creatorPfp: 'ohnepixel-raw.jpg',
+    views: '415k',
+    dateReleased: '2025-01-02'
   }
 ]
 
--- scripts/youtube.js
+++ scripts/youtube.js
@@ -1,4 +1,42 @@
 import { videos } from '../data/video.js'
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 
 
-console.log(videos);
+console.log(dayjs(videos[0].getDateReleased()));
+
+
+const date1 = dayjs('2026-08-16');
+const today = dayjs();
+
+// Get difference in days
+let timeBetween = today.diff(date1, 'day');
+let timeFormat = 'days'
+
+console.log(timeBetween);
+if (timeBetween > 365)
+{
+  timeBetween /= 365;
+  timeFormat = 'year';
+}
+else if (timeBetween < 365 && timeBetween > 30)
+{
+  timeBetween /= 30;
+  timeFormat = 'month';
+}
+else if (timeBetween < 30 && timeBetween > 7)
+{
+  timeBetween /= 7;
+  timeFormat = 'week';
+}
+else if (timeBetween < 7)
+{
+  timeFormat = 'day';
+}
+
+// grammar rules
+if (timeBetween !== 1)
+{
+  timeFormat += 's';
+}
+
+console.log(`${Math.floor(timeBetween)} ${timeFormat}`)
