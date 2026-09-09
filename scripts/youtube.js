7ef652b93720e0f2877d791ba6acdadda00087fc
Andrew Nguyen <andreww.nnguyen@gmail.com>
2026-09-09 09:52:43 -0700

    added shorts menu button to each preview card

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -20,9 +20,29 @@ shuffleArray(shorts);
 // render stuff
 renderVideoGrid(getVideosPerRow(window.innerWidth));
 renderShortsGrid(getShortsPerRow(window.innerWidth));
+setShortsAnchor(getShortsPerRow(window.innerWidth))
 handleScreenSizeSideBar(window.innerWidth);
 renderSideBar();
 
+
+function setShortsAnchor(shortsPerRow)
+{
+  for (let i = 0; i < shortsPerRow; i++)
+  {
+    let short = shorts[i]
+    document.querySelector(`.shorts-menu-button-${short.getShortsCode()}`).style.anchorName
+      = `--${short.getShortsCode()}`
+  
+      document.querySelector(`.shorts-menu-${short.getShortsCode()}`).style.positionAnchor = 
+      `--${short.getShortsCode()}`
+
+      document.querySelector(`.shorts-info-${short.getShortsCode()}`).style.anchorName =
+      `--info-${short.getShortsCode()}`;
+
+      document.querySelector(`.shorts-menu-button-${short.getShortsCode()}`).style.positionAnchor = 
+      `--info-${short.getShortsCode()}`;
+  }
+}
 
 document.querySelectorAll('.js-dismiss-button').forEach((button) =>
 {
@@ -124,6 +144,7 @@ window.addEventListener('resize', () =>
 {
   renderVideoGrid(getVideosPerRow(window.innerWidth));
   renderShortsGrid(getShortsPerRow(window.innerWidth));
+  setShortsAnchor(getShortsPerRow(window.innerWidth))
 });
 
 window.addEventListener('click', (event) =>
@@ -236,19 +257,10 @@ function renderShortsGrid(shortsPerRow)
             <img class="shorts-thumbnail" src="../shorts-thumbnails/${short.getThumbnail()}">
           </div>
 
-          <div class="shorts-info">
+          <div class="shorts-info shorts-info-${short.getShortsCode()} ">
             <div class="shorts-title">
               ${short.getTitle()}
             </div>
-
-            <button class="icon-area shorts-menu-button-5">
-              <svg xmlns="http://www.w3.org/2000/svg"
-              height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true">
-                <path d="M12 4a2 2 0 100 4 2 2 0 000-4Zm0 6a2 2 0 100 4 2 2 0 000-4Zm0 6a2 2 0 100 4 2 2
-                  0 000-4Z">
-                </path>
-              </svg>
-            </button>
           </div>
 
           <div class="shorts-views">
@@ -256,6 +268,72 @@ function renderShortsGrid(shortsPerRow)
           </div>
         </div>
       </a>
+
+      <button popovertarget="shorts-menu-${short.getShortsCode()}" 
+      class="icon-area js-shorts-menu-button shorts-menu-button shorts-menu-button-${short.getShortsCode()}"
+      data-short-code="${short.getShortsCode()}">
+        <svg xmlns="http://www.w3.org/2000/svg"
+        height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true">
+          <path d="M12 4a2 2 0 100 4 2 2 0 000-4Zm0 6a2 2 0 100 4 2 2 0 000-4Zm0 6a2 2 0 100 4 2 2
+            0 000-4Z">
+          </path>
+        </svg>
+      </button>
+
+      <div popover id="shorts-menu-${short.getShortsCode()}" 
+      class="shorts-menu shorts-menu-${short.getShortsCode()}"
+      data-short-code="${short.getShortsCode()}">
+        <div class="pop-up-link">
+          <div class="pop-up-icon-area">
+            <svg xmlns="http://www.w3.org/2000/svg"
+            height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true">
+              <path d="M2 2.864v6.277a.5.5 0 00.748.434L9 6.002 2.748 2.43A.5.5 0 002 2.864ZM21 5h-9a1 1
+                0 100 2h9a1 1 0 100-2Zm0 6H9a1 1 0 000 2h12a1 1 0 000-2Zm0 6H9a1 1 0 000 2h12a1 1 0
+                000-2Z">
+              </path>
+            </svg>
+          </div>
+
+          <div class="pop-up-desc">
+            Add to queue
+          </div>
+        </div>
+
+        <div class="pop-up-link">
+          <div class="pop-up-icon-area">
+            <svg xmlns="http://www.w3.org/2000/svg"
+            height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true">
+              <path d="m4 2.999-.146.073A1.55 1.55 0 003 4.454v16.545a1 1 0 102 0v-6.491a7.26 7.26 0
+                016.248.115l.752.376a8.94 8.94 0 008 0l.145-.073c.524-.262.855-.797.855-1.382V4.458a1.21
+                1.21 0 00-1.752-1.083 7.26 7.26 0 01-6.496 0L12 2.999a8.94 8.94 0 00-8 0Zm7.105
+                1.79v-.002l.752.376A9.26 9.26 0 0019 5.641v7.62a6.95 6.95 0 01-6.105-.052l-.752-.376A9.261 9.261
+                0 005 12.355v-7.62a6.94 6.94 0 016.105.054Z">
+              </path>
+            </svg>
+          </div>
+
+          <div class="pop-up-desc">
+            Report
+          </div>
+        </div>
+
+        <div class="pop-up-link">
+          <div class="pop-up-icon-area">
+            <svg xmlns="http://www.w3.org/2000/svg"
+            height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true">
+              <path d="M19 2H5a4 4 0 00-4 4v10a4 4 0 004 4h2v1.604a1.41 1.41 0 002.095 1.232L14.2 20H19a4
+                4 0 004-4V6a4 4 0 00-4-4ZM5 4h14a2 2 0 012 2v10a2 2 0 01-2 2h-5.318l-.453.252L9 20.6V18H5a2
+                2 0 01-2-2V6a2 2 0 012-2Zm7 2a1 1 0 00-1 1v4.5a1 1 0 002 0V7a1 1 0 00-1-1Zm0 7.75a1.25 1.25
+                0 100 2.5 1.25 1.25 0 000-2.5Z">
+              </path>
+            </svg>
+          </div>
+
+          <div class="pop-up-desc">
+            Send feedback
+          </div>
+        </div>
+      </div>
     `;
   };
 
